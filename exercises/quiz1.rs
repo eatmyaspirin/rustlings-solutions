f5a5a6b94b37c25ac5ca4d26ef3ca105cf58659b
eatmyaspirin <ritvikashesh2000@gmail.com>
2023-03-28 14:57:52 +0530

    Solutions upto 36

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amt : i32) -> i32 {
+    let apple_cost: i32 = 2;
+    if amt <= 40 {
+        amt*apple_cost
+    } else {
+        amt
+    }
+}
 
 // Don't modify this function!
 #[test]
